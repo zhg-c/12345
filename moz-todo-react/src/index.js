e7dfdcbc194aa9c3b5a41e42716acd1550631515
zhg <1640532428@qq.com>
2025-09-26 19:08:11 +0800

    post parameters.

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Edit",completed: true },
+  { id: "todo-1", name: "Sleep",completed: false },
+  { id: "todo-2", name: "Repeat",completed: false },
+];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
